c1099f1c66efc6f8f207ca92e8ac03370022e822
FEvelychko <gitjsdeveloper@gmail.com>
2015-06-20 15:23:28 +0300

    ordering property

--- js/controller.js
+++ js/controller.js
@@ -7,10 +7,15 @@
     rozetkaStore.controller('CarListCtrl', function ($scope) {
         $scope.cars = [
             {'name': '2016 Acura ILX',
-                'description': 'The 2016 Acura ILX has a sportier focus than other entry-luxury sedans, without sacrificing comfort and refinement.'},
+                'description': 'The 2016 Acura ILX has a sportier focus than other entry-luxury sedans, without sacrificing comfort and refinement.',
+                    'age': 0},
             {'name': '2015 Alfa Romeo 4C',
-                'description': 'The 2015 Alfa Romeo 4C is a dazzling sports-car reincarnation of a heady past, and a future classic in its own right.'},
-            {'name': '2015 Aston Martin DB9™',
-                'description': 'The 2015 Aston Martin DB9 is coming soon. You can sign up to receive email updates any time we learn anything new on the Aston Martin DB9.'}
+                'description': 'The 2015 Alfa Romeo 4C is a dazzling sports-car reincarnation of a heady past, and a future classic in its own right.',
+                    'age': 1},
+            {'name': '2014 Aston Martin DB9™',
+                'description': 'The 2015 Aston Martin DB9 is coming soon. You can sign up to receive email updates any time we learn anything new on the Aston Martin DB9.',
+                    'age':2}
         ];
+
+        $scope.orderProp = 'age';
     });
