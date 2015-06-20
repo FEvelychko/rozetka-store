768fc71e38c2ca414549be3496c718a9fb0d7823
FEvelychko <gitjsdeveloper@gmail.com>
2015-06-20 15:52:19 +0300

    data from json

--- js/controller.js
+++ js/controller.js
@@ -4,8 +4,14 @@
 
     var rozetkaStore = angular.module('rozetkaStore', []);
 
-    rozetkaStore.controller('CarListCtrl', function ($scope) {
-        $scope.cars = [
+    rozetkaStore.controller('CarListCtrl', function ($scope, $http) {
+        $http.get('carsData/cars.json').success(function(data) {
+            $scope.cars = data;
+        });
+        $scope.orderProp = 'age';
+
+
+        /*$scope.cars = [
             {'name': '2016 Acura ILX',
                 'description': 'The 2016 Acura ILX has a sportier focus than other entry-luxury sedans, without sacrificing comfort and refinement.',
                     'age': 0},
@@ -15,7 +21,6 @@
             {'name': '2014 Aston Martin DB9™',
                 'description': 'The 2015 Aston Martin DB9 is coming soon. You can sign up to receive email updates any time we learn anything new on the Aston Martin DB9.',
                     'age':2}
-        ];
+        ];*/
 
-        $scope.orderProp = 'age';
     });
